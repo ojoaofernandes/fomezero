91b66a3666da6d8857131e505c2814556c8840ba
João Fernandes <ojoaofernandes@gmail.com>
2017-09-21 22:58:35 -0300

    Add seed to default restaurant and remove create/delete actions.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+Restaurant.create(name: 'Default Restaurant', address: 'Default Address', phone: '555-555-555')
