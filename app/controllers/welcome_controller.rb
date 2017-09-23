d174307efa16e3fc09abf99f1693dedc177108de
João Fernandes <ojoaofernandes@gmail.com>
2017-09-22 21:55:38 -0300

    Add content to the main page. Add some style rules.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    @restaurant = Restaurant.first
+    @plates     = Plate.order(:description).all
     render layout: 'public'
   end
 end
